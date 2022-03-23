4a2505e56cd19154513d004a12ed8dd2b679a54f
SeanGrant18 <sgseangrant@gmail.com>
2022-03-23 12:10:22 +0000

    Added selectors in HTML and Added mulitble Functions in js file to make computer play, make the users input available to see at the top and added incrimented values

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
 let selectionButtons = document.querySelectorAll('[data-selection')
-
+let finalColumn = document.querySelector('[data-final-column]')
+let computerScoreSpan = document.querySelector('[data-computer-score]')
+let yourScoreSpan = document.querySelector('[data-your-score]')
+let SELECTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         let selectionName = selectionButton.dataset.selection
+        let selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
+
+function makeSelection(selection) {
+    let computerSelection = randomSelection()
+    let yourWinner = isWinner(selection, computerSelection)
+    let computerWinner = isWinner(computerSelection, selection)
+    
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner) incermentScore(yourScoreSpan)
+    if (computerWinner) incermentScore(computerScoreSpan)
+}
+
+function incermentScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) +1
+}
+
+function addSelectionResult(selection, winner) {
+    let div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    let randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
+}
